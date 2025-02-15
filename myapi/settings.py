3bf8a033a8a23da25adf0b83879a945824451086
heodabin <2003heo@naver.com>
2025-02-16 01:42:04 +0900

    Feat: settings 수정_20250216

--- myapi/settings.py
+++ myapi/settings.py
@@ -36,9 +36,9 @@ SECRET_KEY=env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG=env('DEBUG')
 DEBUG=False
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
-#ALLOWED_HOSTS=['*'] 개발환경 시 활성화가 편함함
+ALLOWED_HOSTS=['*'] #개발환경 시 활성화가 편함함
 
 # Application definition
 
