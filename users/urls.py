ac49bd621f47fe44a138eb71c0180e338079a26c
heodabin <2003heo@naver.com>
2025-02-22 22:07:05 +0900

    Feat: 회원탈퇴 기능 추가_20250222

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 # users/urls.py
 from django.urls import path
 from . import views
+from .views import UserDeleteView
 
 urlpatterns = [
     # 'me/' 경로에 대한 처리
@@ -9,4 +10,5 @@ urlpatterns = [
     # '<int:pk>/' 경로에 대한 처리 (사용자 정보 가져오기)
     path('<int:pk>/', views.user_detail, name='user_detail'),
     path('account/', views.get_user_account, name='get_user_account'), #사용자별 계좌 잔고가 될 듯함
+    path('delete/', UserDeleteView.as_view(), name='user-delete'),
 ]
