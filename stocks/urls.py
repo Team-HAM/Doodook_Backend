9d4a074d5f7b4898efeba97a1dc41494aac26152
heodabin <2003heo@naver.com>
2025-03-25 17:32:27 +0900

    Fix: 상승 하락 삭제_20250325

--- stocks/urls.py
+++ stocks/urls.py
@@ -3,5 +3,5 @@ from .views import DailyStockPriceView,StockPriceChangeView
 
 urlpatterns = [
     path("daily_stock_price/", DailyStockPriceView.as_view(), name="daily_stock_price"),
-    path("price_change/", StockPriceChangeView.as_view(), name="price_change"),
+    # path("price_change/", StockPriceChangeView.as_view(), name="price_change"),
 ]
