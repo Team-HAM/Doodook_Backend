4ccf009e14a6fdaea5522d6751a57b2d79e753c8
yeonchaepark <check4732@naver.com>
2025-05-27 16:37:16 +0900

    fix:한투 토큰 에러 로그 추가 및 챗봇 에러 해결_250527

--- ai_chatbot/utils.py
+++ ai_chatbot/utils.py
@@ -1,13 +1,13 @@
-import openai
+from openai import OpenAI
 from django.conf import settings
 
-openai.api_key = settings.OPENAI_API_KEY
+client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
 def get_ai_response(user_input):
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "초보 투자자에게 친절하게 존댓말로 설명해줘. 유쾌한 말투로, 예시나 비유도 들어주고고. 300자 이내로 간결하게."},
+            {"role": "system", "content": "초보 투자자에게 친절하게 존댓말로 설명해줘. 유쾌한 말투로, 예시나 비유도 들어주고. 300자 이내로 간결하게."},
             {"role": "user", "content": user_input}
         ],
         temperature=0.7,
